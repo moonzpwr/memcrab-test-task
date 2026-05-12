5cbf91c5bdadcf8a62bd7a55172e0a6acd9a6807
Andrew Yudenkov <yudenkovandre@gmail.com>
2026-05-12 22:01:03 +0300

    fix type

--- src/context/DataContext.ts
+++ src/context/DataContext.ts
@@ -4,7 +4,7 @@ import type { Matrix } from '../types/Matrix';
 type MatrixDataContext = {
 	matrix: Matrix;
 	columnCount: number;
-	x: number;
+	x: number | null;
 };
 
 export const MatrixDataContext = createContext<MatrixDataContext | null>(null);
